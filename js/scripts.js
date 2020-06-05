57ba96c432746f505f4496ca70b85508d2117609
Thomas Glenn <thomasgrantglenn@mac.com>
2020-06-05 11:13:12 -0600

    add back end variable assignments.

--- js/scripts.js
+++ js/scripts.js
@@ -18,41 +18,42 @@ $(document).ready(function() {
 
 
 
-    const remote = 
-    const city
-    const aid
-    const salary
-    const date
-    const hours
+    // this is back-end code:
+    const remote = $("#remote").val();
+    const city = $("#city").val();
+    const aid = $("#aid").val();
+    const salary = $("#salary").val();
+    const day = $("#day").val();
+    const hours = $("#hours").val();
   })
 }
 )
 
 
-$(document).ready(function() {
-  $("#profile").submit(function(event) {
-    const gender = $("#gender").val();
-    const attribute = $("#attribute").val();
+// $(document).ready(function() {
+//   $("#profile").submit(function(event) {
+//     const gender = $("#gender").val();
+//     const attribute = $("#attribute").val();
 
 
 
-    if (gender === 'male' && attribute === 'athletic') {
-      let choice = "Arnold Schwartzenegger";
-      $(".choice").text(choice);
-    } else if (gender === 'male' && attribute === 'caring') {
-      let choice = "Brad Pitt";
-      $(".choice").text(choice);
-    } else if (gender === 'male' && attribute === 'rich') {
-      let choice = "Bill Gates";
-      $(".choice").text(choice);
-    } else if (gender === 'female' && attribute === 'intelligent') {
-      let choice = "Madeleine Albright";
-      $(".choice").text(choice);
-    }
+//     if (gender === 'male' && attribute === 'athletic') {
+//       let choice = "Arnold Schwartzenegger";
+//       $(".choice").text(choice);
+//     } else if (gender === 'male' && attribute === 'caring') {
+//       let choice = "Brad Pitt";
+//       $(".choice").text(choice);
+//     } else if (gender === 'male' && attribute === 'rich') {
+//       let choice = "Bill Gates";
+//       $(".choice").text(choice);
+//     } else if (gender === 'female' && attribute === 'intelligent') {
+//       let choice = "Madeleine Albright";
+//       $(".choice").text(choice);
+//     }
 
-    $("#date").text();
-    event.preventDefault();
+//     $("#date").text();
+//     event.preventDefault();
 
-  });
-});
+//   });
+// });
 
